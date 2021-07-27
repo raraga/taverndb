1524e99d1981a8ee3c404cfefd5f4a681618adc1
Renato Araga <raraga@gmail.com>
2021-07-27 00:20:48 -0700

    added PostGres support

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -70,9 +70,9 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd1udiut0tunp33'),
+            'username' => env('DB_USERNAME', 'mbtlayjpjahwgu'),
+            'password' => env('DB_PASSWORD', 'aaa176c1e0422e195ec8f3379d6db39bb289576742e1c94fdeaef191c0bd3c1c'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
